179f450430cf3a46e39a499566e8bee4bf6f2152
oXg3n <a.perrier721@gmail.com>
2016-11-01 01:08:25 +0100

    mail test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,4 +7,12 @@ Route::group(['middleware' => ['web']], function () {
 	Route::post('/', [
 	    'as' => 'store', 'uses' => 'HomeController@store'
 	]);
+
+	Route::get('/contact', function () {
+    	Mail::send('emails.welcome', ['email' => $email], function ($message) use ($email) {
+            $message->from('contact@chez-robert.fr', 'Chez Robert');
+            $message->sender('contact@chez-robert.fr', 'Chez Robert');
+            $message->to($email)->subject("Bienvenue Chez Robert");
+        });
+	});
 });
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,1 +1,15 @@
-Test
+<!doctype html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<p>Bienvenue Chez Robert</p>
+
+<a href="https://chez-robert.fr" title="Chez Robert"><img id="logo" src="http://chez-robert.fr/images/logo-robert.png" alt="Chez Robert"></a>
+<p>Merci de vous être abonné.</p>
+
+<p>Nous vous remercions de vous être inscrit !</p>
+
+</body>
+</html>
